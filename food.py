4cbd013791c7bce4ee5ab8d7f9c4e7fa34cad0d9
red095 <redietteklay8@gmail.com>
2026-02-07 21:25:19 +0300

    feat: Randomize food color and update it on refresh.

--- food.py
+++ food.py
@@ -1,18 +1,30 @@
 import random
-from turtle import Turtle, Screen
-
+from turtle import Turtle, colormode
+colormode(255)
 
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.shapesize(0.2)
         self.penup()
-        self.color("red")
+        self.set_color(self.get_random_color())
         self.speed("fastest")
         self.refresh()
 
+
+    def get_random_color(self):
+        """Generates a random RGB color tuple."""
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        return (r, g, b)
+    def set_color(self, color):
+        self.color(color)
+
     def refresh(self):
+        self.set_color(self.get_random_color())
         rand_x = random.randint(-280, 280)
         rand_y = random.randint(-280, 280)
         self.goto(rand_x, rand_y)
